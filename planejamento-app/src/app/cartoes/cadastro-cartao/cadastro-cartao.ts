8b1f25f4baa241fbde37ea502715a6f03d6cb44e
DiogoVSF07 <diogosampaio07@gmail.com>
2026-09-06 00:22:19 -0300

    [PLAN-1] commit 4 - criando front

---
+++ planejamento-app/src/app/cartoes/cadastro-cartao/cadastro-cartao.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, inject} from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CartaoService } from '../cartao-service';
+import { DadosCartaoFrom, DetalhesCartao } from '../dados-cartao';
+
+interface CadastroCartaoForm {
+  nome: FormControl<string>;
+  bandeira: FormControl<string>;
+}
+
+@Component({
+  selector: 'app-cadastro-cartao',
+  imports: [ReactiveFormsModule],
+  templateUrl: './cadastro-cartao.html',
+  styleUrl: './cadastro-cartao.scss',
+})
+export class CadastroCartao implements OnInit{
+
+  form!: FormGroup<CadastroCartaoForm>;
+  service = inject(CartaoService);
+
+  ngOnInit(): void {
+    this.form = new FormGroup<CadastroCartaoForm>({
+      nome: new FormControl('', {nonNullable: true, validators: Validators.required}),
+      bandeira: new FormControl('', {nonNullable: true, validators: Validators.required })
+    });
+  }
+
+  handleSubmit(){
+    console.log(this.form.value);
+    const dadosCartao = this.form.value as DadosCartaoFrom;
+    this.service
+        .criar(dadosCartao)
+        .subscribe({
+          next: (response: DetalhesCartao) => {
+            console.log('recebendo a resposta do servidor:', response);
+          },
+          error: (error) => console.log('ocorreu um erro ', error)
+        });
+  }
+}
---
+++ planejamento-app/src/app/cartoes/dados-cartao.ts
@@ -0,0 +1,11 @@
+export class DadosCartaoFrom{
+    nome!: string;
+    bandeira!: string;
+}
+
+export class DetalhesCartao{
+    id!: string;
+    nome!: string;
+    bandeira!:string;
+    dataCadastro!:Date;
+}

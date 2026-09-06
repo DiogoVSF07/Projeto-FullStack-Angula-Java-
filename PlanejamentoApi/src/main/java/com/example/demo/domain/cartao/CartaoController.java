ca03e8a1e4d6369c99fa0eddcf0dc057ce23c2fa
DiogoVSF07 <diogosampaio07@gmail.com>
2026-09-06 00:21:14 -0300

    [PLAN-1] commit 3 - ajustando configuração

--- PlanejamentoApi/src/main/java/com/example/demo/domain/cartao/CartaoController.java
+++ PlanejamentoApi/src/main/java/com/example/demo/domain/cartao/CartaoController.java
@@ -1,4 +1,60 @@
 package com.example.demo.domain.cartao;
 
+import com.example.demo.domain.cartao.dto.CartaoDetalhes;
+import com.example.demo.domain.cartao.dto.CartaoForm;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("cartoes")
+@CrossOrigin(origins = "*")
 public class CartaoController {
+
+    @Autowired
+    private CartaoService service;
+
+    @PostMapping
+    public ResponseEntity<CartaoDetalhes> criar(
+            @RequestBody @Valid CartaoForm novo){
+        CartaoDetalhes detalhes = service.criar(novo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(detalhes);
+    }
+
+    // dominio/cartoes/difaudifuaidufia
+    @GetMapping("{id}")
+    public ResponseEntity<CartaoDetalhes> obterDetalhes(@PathVariable("id") UUID id){
+        var result = service.obterDetalhes(id);
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(
+            @PathVariable UUID id, @RequestBody CartaoForm dadosAtualizacao){
+        service.atualizar(id, dadosAtualizacao);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public Page<CartaoDetalhes> listar(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size
+    ){
+        var pageRequest = PageRequest.of(page, size);
+        return service.listar(pageRequest);
+    }
+
+    // put: http://dominio/cartoes/difaudifuaidufia/status
+    @PatchMapping("{id}/status")
+    public ResponseEntity<Void> mudarStatus(@PathVariable UUID id){
+        service.mudarStatus(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
